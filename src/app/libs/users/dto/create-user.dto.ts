88c2821e04ee89b0a8066db1b7da80c0067e6938
akavi <gabdushev.i@yandex.ru>
2022-07-13 18:10:17 +0500

    (feat) add validation to dto

--- src/app/libs/users/dto/create-user.dto.ts
+++ src/app/libs/users/dto/create-user.dto.ts
@@ -1,7 +1,31 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(4, 16)
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly surname: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
+
+  @IsString()
+  @Length(4, 16)
+  @IsNotEmpty()
   readonly password: string;
+
+  @IsOptional()
+  @IsString()
   readonly phone: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
